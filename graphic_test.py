3a2bb56b9c7f737b19e02c1bd4cc37ca41b27897
Luca Toscano <luca.toscano4@studio.unibo.it>
2020-06-08 21:12:08 +0200

    first window using Tkinter

---
+++ graphic_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 15:12:17 2020
+
+@author: lucatoscano
+""" 
+
+from tkinter import *
+
+#create the window
+window= Tk()
+ 
+#size of window
+
+window.geometry ('500x500+500+700') #width, height, coordinate x and y (pixel)
+window.title("my first window")
+#start the main loop
+
+window.mainloop()
