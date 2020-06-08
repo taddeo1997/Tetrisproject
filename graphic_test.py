6c99fb6c080809e2bce4e07fc5f67fa74d1d6f67
Luca Toscano <luca.toscano4@studio.unibo.it>
2020-06-08 21:29:21 +0200

    add file containing first window using Tkinter

--- graphic_test.py
+++ graphic_test.py
@@ -12,9 +12,10 @@ from tkinter import *
 window= Tk()
  
 #size of window
-
 window.geometry ('500x500+500+700') #width, height, coordinate x and y (pixel)
-window.title("my first window")
-#start the main loop
 
+#title of window
+window.title("my first window")
+
+#start the main loop
 window.mainloop()
