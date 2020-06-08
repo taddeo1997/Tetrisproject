0aa9b2063afa7bc3932585c643c39b8d68f54202
Luca Toscano <luca.toscano4@studio.unibo.it>
2020-06-08 15:47:36 +0200

    added the game window

---
+++ window.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 15:12:17 2020
+
+@author: lucatoscano
+"""
+base= float(input("dammi la base: "))
+esponente=float(input("dammi l'esponente"))
+print("il risultato di ",base,"elevato alla ",esponente," è ",base**esponente)
