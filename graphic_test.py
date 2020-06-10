55f3767c7478c94805d08009f0b2568e92daf930
Luca Toscano <luca.toscano4@studio.unibo.it>
2020-06-10 15:19:04 +0200

    game_ground with first graphic

--- graphic_test.py
+++ graphic_test.py
@@ -19,15 +19,36 @@ window.geometry ('500x500+500+700') #width, height, coordinate x and y (pixel)
 window.title("my first window")
 
 #adding test
-text=tk.Label(window,text="TETRIS",fg="blue",bg="green",font=("Helvetica",27)).pack()  #pack method places text in central position
+
+#pack method places text in central position
+testo1=tk.Label(window,text="TETRIS Verde",fg="black",bg="green",font=("Helvetica",27)).pack(fill=tk.X)  
+testo2=tk.Label(window,text="TETRIS Bianco",fg="black",bg="white",font=("Helvetica",27)).pack(fill=tk.X)
+#testo3=tk.Label(window,text="TETRIS Rosso",fg="black",bg="red",font=("Helvetica",27)).pack(fill=tk.X)  
+
+#testo1=tk.Label(window,text="TETRIS Verde",fg="black",bg="green",font=("Helvetica",27)).grid(row=0, column=0, sticky=tk.W)  
+#testo2=tk.Label(window,text="TETRIS Bianco",fg="black",bg="white",font=("Helvetica",27)).grid(row=0, column=1, sticky=tk.W)
+#testo3=tk.Label(window,text="TETRIS Rosso",fg="black",bg="red",font=("Helvetica",27)).grid(row=1, column=0, sticky=tk.E)  
+#testo3=tk.Label(window,text="TETRIS Blu",fg="black",bg="blue",font=("Helvetica",27)).grid(row=1, column=1, sticky=tk.E) 
+
+#Create menu
+
+menu_bar=tk.Menu(window).add_cascade(label="file")
+menu_file=tk.Menu(menu_bar)
+menu_file.add_command(Label="New Game")
+menu_file.add_command(Label="Resume")
+menu_file.add_command(Label="Salve")
+menu_file.add_command(Label="Exit")
+
+window.config(Menu=menu_bar)
 
 #function of the botton
-def button1_function():
-    button_text=tk.Label(window,text="the button work well").pack()
+#def button1_function():
+#    button_text=tk.Label(window,text="the button work well").pack()
 
 #add botton
-button1=tk.Button(window,text="unuseful",command=button1_function).pack()
+#button1=tk.Button(window,text="unuseful",command=button1_function).pack()
 
+#C1=tk.Canvas
 
 #start the main loop
 window.mainloop()
